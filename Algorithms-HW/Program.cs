d916eb89df10fba4c0d8b1c006fe3a9d61601d6f
logan Parkhurst <loganold@hotmail.com>
2024-09-16 19:07:56 -0500

    Created File

---
+++ Algorithms-HW/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Algorithms_HW;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Week1
+{
+    static void Constant()
+    {
+        
+    }
+}
---
+++ Algorithms-Week-2-notes/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Algorithms_Week_2_notes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
