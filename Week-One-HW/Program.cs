77b0ce0832c2888c28e561ef02c3192e864846bb
logan Parkhurst <loganold@hotmail.com>
2024-09-20 13:09:58 -0500

    week 1 HW oops

---
+++ Week-One-HW/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Week_One_HW;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> data = ["Hello, World!", "Wow Second!", "Wow Third!"];
+        Console.WriteLine(data);
+        Console.WriteLine("\n\rConstant: ");
+        Constant(data);
+        Console.WriteLine("\n\rLinear: ");
+        Linear(data);
+        Console.WriteLine("\n\rQuadratic: ");
+        Quadratic(data);
+    }
+
+    static void Constant(List<String> args)
+    {
+        Console.WriteLine(args[0]);
+    }
+
+    static void Linear(List<String> args)
+    {
+        foreach (var s in args)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    static void Quadratic(List<String> args)
+    {
+        foreach (var s in args)
+        {
+            foreach (var v in args)
+            {
+                Console.WriteLine(s + " " + v);
+            }
+        }
+    }
+}
