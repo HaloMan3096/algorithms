52ea0dbb171cd5bf11268f7a190d9f80f9d3fa78
logan <Loganold@hotmail.com>
2024-10-10 01:25:34 -0400

    Created the game of war and set up for future card game

--- AlgorithmsLibrary/CardFunctions.cs
+++ AlgorithmsLibrary/CardFunctions.cs
@@ -1,4 +1,6 @@
-﻿namespace AlgorithmsLibrary;
+﻿using System.Collections;
+
+namespace AlgorithmsLibrary;
 using System.Linq;
 using System.Collections.Generic;
 
@@ -14,6 +16,38 @@ public static class CardFunctions
                 Select(x => x.Item).
                 Take(size);
         return shuffledList;
+    }
+
+    public static IEnumerable<T> FisherShuffle<T>(IEnumerable<T> cards, int size)
+    {
+        var rng = new Random();
+        for (int i = size; i < 0; i--)
+        {
+            int j = rng.Next(0, i);
+            cards = Swap(cards.ToList(), i, j);
+        }
+        return cards;
+    }
+
+    public static IEnumerable<T> ReverseFisherShuffle<T>(IEnumerable<T> cards, int size)
+    {
+        var rng = new Random();
+        for (int i = 0; i < size - 2; i++)
+        {
+            var j = rng.Next(i, size);
+            cards = Swap(cards.ToList(), i, j);
+        }
+        return cards;
+    }
+
+    private static IEnumerable<T> Swap<T>(List<T> list, int i, int j)
+    {
+        var tmp1 = list.ElementAt(i);
+        var tmp2 = list.ElementAt(j);
+        list[i] = tmp2;
+        list[j] = tmp1;
+        
+        return list;
     }
 
     public static (IEnumerable<T>, IEnumerable<T>) Split<T>(IEnumerable<T> cards, bool shuffle = false)
